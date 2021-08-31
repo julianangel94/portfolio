b4471933af1f6ebd75f8bc85bdfb1abc6a51d3a2
Julian Angel <julian.arango@sourcecol.com>
2021-08-31 17:54:03 -0500

    Typing

---
+++ components/Typing/index.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+const words = ["Dev", "Pro"];
+
+export default function Typing() {
+  const [index, setIndex] = useState(0);
+  const [subIndex, setSubIndex] = useState(0);
+  const [blink, setBlink] = useState(true);
+  const [reverse, setReverse] = useState(false);
+
+  // typeWriter
+  useEffect(() => {
+    if (index === words.length) return;
+
+    if (
+      subIndex === words[index].length + 1 &&
+      index !== words.length - 1 &&
+      !reverse
+    ) {
+      setReverse(true);
+      return;
+    }
+
+    if (subIndex === 0 && reverse && index < words.length - 1) {
+      setReverse(false);
+      setIndex((prev) => prev + 1);
+      return;
+    }
+
+    if (subIndex === 0 && reverse) {
+      setReverse(false);
+      setIndex((prev) => prev - 1);
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setSubIndex((prev) => prev + (reverse ? -1 : 1));
+    }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 : 150, parseInt(Math.random() * 350)));
+
+    return () => clearTimeout(timeout);
+  }, [subIndex, index, reverse]);
+
+  // blinker
+  useEffect(() => {
+    const timeout2 = setTimeout(() => {
+      setBlink((prev) => !prev);
+    }, 500);
+    return () => clearTimeout(timeout2);
+  }, [blink]);
+
+  useEffect(() => {
+    if (
+      index === words.length - 1 &&
+      subIndex === words[index].length + 1 &&
+      !reverse
+    ) {
+      setReverse(true);
+    }
+  }, [subIndex]);
+
+  return (
+    <>
+      <h1>
+        The {`${words[index].substring(0, subIndex)}${blink ? "|" : " "}`}
+      </h1>
+      <style jsx>{`
+        h1 {
+          color: white;
+        }
+      `}</style>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
+import Typing from "components/Typing";
+
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <Typing />
+    </div>
+  );
 }
